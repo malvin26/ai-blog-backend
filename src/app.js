a489c1949c2f5f4dcaa6093c10d2f5bad4690713
malvin <malvinjack26@gmail.com>
2026-07-16 00:28:32 +0600

    hello code

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://ai-blog-frontend-0t4a.onrender.com/"
+            "https://ai-blog-frontend-0t4a.onrender.com"
         ],
         credentials: true,
     })
