c409bea3b276b2dcf360612a71ec9e3ad1739bbe
malvin <malvinjack26@gmail.com>
2026-07-16 00:15:11 +0600

    update cors policy

--- src/app.js
+++ src/app.js
@@ -14,7 +14,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: [
+            "http://localhost:5173",
+            "https://ai-blog-frontend-0t4a.onrender.com/"
+        ],
         credentials: true,
     })
 );
