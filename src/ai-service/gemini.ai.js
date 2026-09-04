af30e8f5f7c1347baa09ff2d90965a3ce88478ac
malvin <malvinjack26@gmail.com>
2026-09-04 10:36:33 +0600

    90709jtrhuhuy476464

--- src/ai-service/gemini.ai.js
+++ src/ai-service/gemini.ai.js
@@ -74,7 +74,7 @@ export const gemini = async ({
     }
 
     const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
         {
             method: "POST",
 
