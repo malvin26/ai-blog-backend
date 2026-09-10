bdb592a8f04514609184c29b7922496b6786d26e
malvin <malvinjack26@gmail.com>
2026-09-10 21:20:27 +0600

    08790kukuik7iu970

--- src/app.js
+++ src/app.js
@@ -36,9 +36,9 @@ app.use(
 ===================================================== */
 
 const allowedOrigins = [
-    "https://ai-blog-frontend-0t4a.onrender.com",
-    // "http://localhost:5173",
-    "https://www.fastblog.online",
+    // "https://ai-blog-frontend-0t4a.onrender.com",
+    // // "http://localhost:5173",
+    // "https://www.fastblog.online",
     "https://www.fastblog.online",
 ];
 
